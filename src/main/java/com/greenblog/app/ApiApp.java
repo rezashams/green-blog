118caea4e0e07c32f48b13a5cc65ede49f39d6f7
rezashams <rezashams86@gmail.com>
2020-08-18 23:06:05 +0430

    set up project

---
+++ src/main/java/com/greenblog/app/ApiApp.java
@@ -0,0 +1,19 @@
+/*
+ *
+ *
+ * Created by IntelliJ IDEA.
+ * @author: Reza Shams <rezashams86@gmail.com>
+ */
+package com.greenblog.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApp {
+
+    public static  void main(String[] args) {
+        SpringApplication.run(ApiApp.class,args);
+    }
+
+}
